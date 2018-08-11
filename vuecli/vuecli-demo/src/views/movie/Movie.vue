721800c57e8d58ced00acba6ce1a2dd52f0d3cb7
fengchuanyu <fengchuanyu@163.com>
2018-08-11 19:45:21 +0800

    vue

--- vuecli/vuecli-demo/src/views/movie/Movie.vue
+++ vuecli/vuecli-demo/src/views/movie/Movie.vue
@@ -38,7 +38,7 @@ export default {
   methods:{
     getData(){
       this.isFinish = false;
-      axios.get('https://api.myjson.com/bins/nsb9g')
+      axios.get(API_PROXY+'https://api.douban.com/v2/movie/in_theaters?start='+this.dataList.length+'&count=10')
       .then((response) => {
         this.dataList = this.dataList.concat(response.data.subjects)
         this.isFinish = true;
